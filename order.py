3042dfaac914a1a0bd623c58da3c108555d0bd3d
MANSOORKHAN71 <101921374+MANSOORKHAN71@users.noreply.github.com>
2022-07-04 21:05:20 +0500

    Add files via upload

---
+++ order.py
@@ -0,0 +1,15 @@
+print("Welcome to my Cafe ")
+
+name=input("Whats your Name?")
+
+print("Hello " + name + ", Thank you so much for comming.")
+
+menu="bbq wings, roast chicken ,biryani , chai"
+
+print(name + " what would you like from our menu \n" + menu)
+
+order=input()
+
+print(name + " your " + order + " will be served in an hour ")
+
+print("how was the " +  order + name)
