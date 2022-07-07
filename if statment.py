f83dc1271e59440055f2f07515f5c0a77ff9d9e2
MANSOORKHAN71 <101921374+MANSOORKHAN71@users.noreply.github.com>
2022-07-07 14:16:14 +0500

    Update if statment.py

--- if statment.py
+++ if statment.py
@@ -1,13 +1,13 @@
-temprature= 25
+temprature=input("Whats the temprature")
 
-if temprature > 30:
+if int(temprature) > 30:
     print("It's a hot day")
     print("Then drink plenty of water")
     
-elif temprature > 20:
+elif int(temprature) > 20:
     print("Its a pleasent day")
     
-elif temprature > 10:
+elif int(temprature) > 10:
     print("it's a bit cold")    
     
 else :
