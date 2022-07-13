eb4fc71daf4f4697998c2757114230c4c9f0755c
MANSOORKHAN71 <101921374+MANSOORKHAN71@users.noreply.github.com>
2022-07-13 19:29:09 +0500

    Add files via upload

---
+++ Starbucks.py
@@ -0,0 +1,35 @@
+print("Hello! Welcome to starbucks\n \n")
+      
+name=input("Whats your Name \n")
+
+drinks ="latte, Espresso, Capuccino, Black Coffe, Simple coffe, Cold Cofee"
+Donuts
+
+
+print( name + " what will you like to try \n" + drinks )
+
+order=input()
+
+price= 30
+
+quantity=input("How much " + order +" you want  \n"  )
+
+total= price * int(quantity)
+
+
+
+print("Your total is : \n" + str(total) )
+
+print(" \nYour " + quantity + order +" Will be ready in a moment \n \n")
+
+taste=input("How was the taste  \n" )
+
+if  taste =="bad" :
+    print("We will try to improve " + name )
+else :
+    print("Thanks for your great review \n")
+
+print("Thanks For Comming ")
+
+
+
